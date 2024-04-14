d2e9a7ac9590736deb246fef29f31282531008ea
iacreative <ia2design@hotmail.com>
2024-04-14 15:27:49 -0300

    :recycle: refactor: Refactor config file

--- config/laravel-more-command.php
+++ config/laravel-more-command.php
@@ -40,4 +40,46 @@ return [
     |
     */
     'trait-namespace' => 'App',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Repository classes namespace
+    |--------------------------------------------------------------------------
+    |
+    | This value defines the default namespace for created Repository classes.
+    | For example if the value is 'Module/App/Http', it will create repository classes
+    | inside 'Module/App/Http/Repositories' and class namespace will
+    | 'Module/App/Http/Repositories/{ClassName}'.
+    |
+    */
+
+    'module-repository-namespace' => 'App/Test',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Service classes namespace
+    |--------------------------------------------------------------------------
+    |
+    | This value defines the default namespace for created Service classes.
+    | For example if the value is 'Module/App/Http', it will create repository classes
+    | inside 'Module/App/Http/Services' and class namespace will
+    | 'Module/App/Http/Services/{ClassName}'.
+    |
+    */
+    
+    'module-service-namespace' => 'App',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Module Trait classes namespace
+    |--------------------------------------------------------------------------
+    |
+    | This value defines the default namespace for created Trait classes.
+    | For example if the value is 'Module/App/Http', it will create repository classes
+    | inside 'App/Http/Traits' and class namespace will
+    | 'Module/App/Http/Traits/{ClassName}'.
+    |
+    */
+    
+    'module-trait-namespace' => 'App',
 ];
